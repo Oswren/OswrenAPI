9c03b4ddf0dba2bdb1082e44c46105cb527342e4
Toby Brown <toby.brown@microlise.com>
2021-02-20 22:13:05 +0000

    cleans up unused usings and adds missing "v1" to api configs

--- OswrenAPI.Domain/Models/DomainServiceCollection.cs
+++ OswrenAPI.Domain/Models/DomainServiceCollection.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using OswrenAPI.Domain.Interfaces;
 using OswrenAPI.Domain.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OswrenAPI.Domain.Models
 {
