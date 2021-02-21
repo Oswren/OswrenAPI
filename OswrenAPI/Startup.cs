1df35c2f96ce996e50d3423aded406cff7c2fbcd
Toby Brown <toby.brown@microlise.com>
2021-02-21 11:20:31 +0000

    minor changes for deployment

--- OswrenAPI/Startup.cs
+++ OswrenAPI/Startup.cs
@@ -42,9 +42,10 @@ namespace OswrenAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OswrenAPI v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OswrenAPI v1"));
 
             app.UseHttpsRedirection();
 
